3aa740b7b36d2f84e531d72dac82d705ede07124
Travis Briggs <audiodude@gmail.com>
2017-06-26 02:15:40 +0000

    Formatting

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
-import sys, pygame
+import sys
+
+import pygame
 pygame.init()
 
 width, height = 800, 480
@@ -11,16 +13,16 @@ pygame.mouse.set_visible(False)
 ball = pygame.image.load("ball.gif")
 ballrect = ball.get_rect()
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT: sys.exit()
+    
+  ballrect = ballrect.move(speed)
+  if ballrect.left < 0 or ballrect.right > width:
+    speed[0] = -speed[0]
+  if ballrect.top < 0 or ballrect.bottom > height:
+    speed[1] = -speed[1]
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+  screen.fill(black)
+  screen.blit(ball, ballrect)
+  pygame.display.flip()
