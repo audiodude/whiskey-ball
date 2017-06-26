b94afefdf15a7c4690e446addfa8f3fd7cf756ea
Travis Briggs <audiodude@gmail.com>
2017-06-26 08:15:14 -0700

    Formatting and allow the user to press ESC to quit.

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
-import sys, pygame
+import sys
+
+import pygame
+
 pygame.init()
 
 width, height = 800, 480
@@ -11,16 +14,20 @@ pygame.mouse.set_visible(False)
 ball = pygame.image.load("ball.gif")
 ballrect = ball.get_rect()
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_ESCAPE:
+        sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+  ballrect = ballrect.move(speed)
+  if ballrect.left < 0 or ballrect.right > width:
+    speed[0] = -speed[0]
+  if ballrect.top < 0 or ballrect.bottom > height:
+    speed[1] = -speed[1]
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+  screen.fill(black)
+  screen.blit(ball, ballrect)
+  pygame.display.flip()
