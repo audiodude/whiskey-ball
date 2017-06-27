8a9f44ddf9ead4728e7d4fb1790e3084b161b573
Travis Briggs <audiodude@gmail.com>
2017-06-26 17:30:25 -0700

    Basic GAME OVER display in two parts.

--- game.py
+++ game.py
@@ -3,17 +3,38 @@ import sys
 import pygame
 
 pygame.init()
-
-width, height = 800, 480
-speed = [2, 2]
-black = 0, 0, 0
-
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 pygame.mouse.set_visible(False)
 
-ball = pygame.image.load("ball.gif")
-ballrect = ball.get_rect()
+width, height = 1024, 600
+half = width, height//2
+clr_grey = pygame.Color('#DDDDDD')
+clr_neon_blue = pygame.Color('#BBFFFF')
+clr_neon_pink = pygame.Color('#FF69B4')
+clr_neon_green = pygame.Color('#59FF3A')
+fnt_default_400 = pygame.font.Font(None, 400)
 
+area = pygame.Surface((width, height))
+
+class GameOverDisplay(object):
+  top = pygame.Surface(half)
+  bottom = pygame.Surface(half)
+  txt_game = fnt_default_400.render('GAME', 1, clr_neon_pink)
+  txt_over = fnt_default_400.render('OVER', 1, clr_neon_pink)
+
+  def __init__(self):
+    top_color = clr_neon_blue
+    bottom_color = clr_neon_green
+    self.top.fill(top_color)
+    self.bottom.fill(bottom_color)
+
+  def draw(self):
+    self.top.blit(self.txt_game, (80, 30))
+    self.bottom.blit(self.txt_over, (110, 30))
+    screen.blit(self.top, (0, 0))
+    screen.blit(self.bottom, (0, height//2))
+
+game_over = GameOverDisplay()
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -22,12 +43,6 @@ while True:
       if event.key == pygame.K_ESCAPE:
         sys.exit()
 
-  ballrect = ballrect.move(speed)
-  if ballrect.left < 0 or ballrect.right > width:
-    speed[0] = -speed[0]
-  if ballrect.top < 0 or ballrect.bottom > height:
-    speed[1] = -speed[1]
-
-  screen.fill(black)
-  screen.blit(ball, ballrect)
+  screen.fill(clr_grey)
+  game_over.draw()
   pygame.display.flip()
