28a8b14318670ae3beaaec74ae72a195a94c127f
Travis Briggs <audiodude@gmail.com>
2017-07-01 12:15:13 -0700

    Animate scoring and center it.

--- game.py
+++ game.py
@@ -67,14 +67,45 @@ class GameOverDisplay(object):
     if keycode == pygame.K_SPACE:
       self.game.goto_main()
 
+class ScoreAnimation(object):
+  def __init__(self, surface, score, fill_color=clr_black):
+    self.surface = surface
+    self.fill_color = fill_color
+    self.elapsed = 0
+    self.cycles = 0
+    self.score = score
+    self.showing = True
+
+  def draw(self):
+    if self.showing:
+      score_str = str(self.score)
+      x = 340
+      if len(score_str) == 3:
+        x = 260
+      txt_scored = fnt_default_400.render(score_str, 1, clr_neon_green)
+      self.surface.blit(txt_scored, (x, 20))
+    else:
+      self.surface.fill(self.fill_color)
+
+  def update(self, tick):
+    if self.cycles >= 10:
+      self.showing = False
+      return
+
+    self.elapsed += tick
+    if (self.cycles > 1 and self.elapsed > 80) or self.elapsed > 400:
+      self.elapsed = 0
+      self.cycles += 1
+      self.showing = not self.showing
+
 class MainDisplay(object):
   def __init__(self, game):
     self.game = game
     self.top = pygame.Surface(half)
     self.bottom = pygame.Surface(half)
     self.txt_score_header = fnt_default_200.render('score:', 1, clr_neon_pink)
-    self.txt_time_header = fnt_default_200.render('time:', 1, clr_white)
-    self.animate_score = 0
+    self.txt_time_header = fnt_default_200.render('time:', 1, clr_black)
+    self.animate_score = None
 
   def reset(self, total_time=40):
     self.rem_secs = total_time
@@ -82,13 +113,12 @@ class MainDisplay(object):
 
   def draw(self):
     txt_score = fnt_default_300.render(str(game.score), 1, clr_neon_pink)
-    txt_time = fnt_default_300.render(str(self.rem_secs), 1, clr_white)
+    txt_time = fnt_default_300.render(str(self.rem_secs), 1, clr_black)
     self.top.fill(clr_neon_blue)
-    self.bottom.fill(clr_black)
     if self.animate_score:
-      txt_scored = fnt_default_300.render(
-        str(self.animate_score), 1, clr_neon_blue)
-      self.bottom.blit(txt_scored, (40, 20))
+      self.animate_score.draw()
+    else:
+      self.bottom.fill(clr_black)
 
     self.top.blit(self.txt_score_header, (10, 0))
     self.top.blit(self.txt_time_header, (640, 0))
@@ -98,6 +128,9 @@ class MainDisplay(object):
     screen.blit(self.bottom, (0, height//2))
 
   def update(self, tick):
+    if self.animate_score:
+      self.animate_score.update(tick)
+
     self.elapsed += tick
     if self.elapsed > 1000:
       self.elapsed = 0
@@ -110,7 +143,7 @@ class MainDisplay(object):
     plus_score = scoremap.get(string, 0)
     if plus_score:
       self.game.score += plus_score
-      self.animate_score = plus_score
+      self.animate_score = ScoreAnimation(self.bottom, plus_score)
 
 class Game(object):
   def __init__(self):
@@ -127,7 +160,7 @@ class Game(object):
     self.current_state.draw()
 
   def update(self):
-    tick = self.clock.tick()
+    tick = self.clock.tick(30)
     result = self.current_state.update(tick)
 
   def goto_main(self):
