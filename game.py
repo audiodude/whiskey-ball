169de365048b381bcd6f2497ed14153ecd92dc34
Travis Briggs <audiodude@gmail.com>
2017-07-15 21:33:17 -0700

    Add numbers

--- game.py
+++ game.py
@@ -595,8 +595,9 @@ class Initials(object):
   checkmark = '✓'
   alphabet = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '-',
-    '$', '&', '*', '(', ')', space, delete,
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
+    '4', '5', '6', '7', '8', '9', '!', '@', '#', '-', '$', '&', '*', '(', ')',
+    space, delete,
   ]
   last_spot = [checkmark, delete]
 
